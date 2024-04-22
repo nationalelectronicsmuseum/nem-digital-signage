dedaef2799ace653e4851561d9bb0a93a74024f5
Daniel Cha <cha.danieldev@gmail.com>
2024-04-22 14:44:53 -0400

    adjusted div boxes and main menu

--- src/pages/homeStation.jsx
+++ src/pages/homeStation.jsx
@@ -7,44 +7,20 @@ function HomeStation() {
       <div>
         <h1 className="title">National Electronics Museum</h1>
       </div>
-      <div>
-        <Link to="/station1" relative="path">
-          Station 1
-        </Link>
-        <Link to="/station2" relative="path">
-          Station 2
-        </Link>
-        <Link to="/station3" relative="path">
-          Station 3
-        </Link>
-        <Link to="/station4" relative="path">
-          Station 4
-        </Link>
-        <Link to="/station5" relative="path">
-          Station 5
-        </Link>
-        <Link to="/station6" relative="path">
-          Station 6
-        </Link>
-        <Link to="/station7" relative="path">
-          Station 7
-        </Link>
-        <Link to="/station8" relative="path">
-          Station 8
-        </Link>
-        <Link to="/station9" relative="path">
-          Station 9
-        </Link>
-        <Link to="/station10" relative="path">
-          Station 10
-        </Link>
-        <Link to="/station11" relative="path">
-          Station 11
-        </Link>
-        <Link to="/station12" relative="path">
-          Station 12
-        </Link>
-      </div>
+      <ul>
+        <li><Link to="/station1" relative="path" className="homeImg">Station 1</Link></li>
+        <li><Link to="/station2" relative="path" className="homeImg">Station 2</Link></li>
+        <li><Link to="/station3" relative="path" className="homeImg">Station 3</Link></li>
+        <li><Link to="/station4" relative="path" className="homeImg">Station 4</Link></li>
+        <li><Link to="/station5" relative="path" className="homeImg">Station 5</Link></li>
+        <li><Link to="/station6" relative="path" className="homeImg">Station 6</Link></li>
+        <li><Link to="/station7" relative="path" className="homeImg">Station 7</Link></li>
+        <li><Link to="/station8" relative="path" className="homeImg">Station 8</Link></li>
+        <li><Link to="/station9" relative="path" className="homeImg">Station 9</Link></li>
+        <li><Link to="/station10" relative="path" className="homeImg">Station 10</Link></li>
+        <li><Link to="/station11" relative="path" className="homeImg">Station 11</Link></li>
+        <li><Link to="/station12" relative="path" className="homeImg">Station 12</Link></li>
+      </ul>
     </>
   );
 }
