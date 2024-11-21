45c034cdbafa8bd84ff2fce7aa162a5bc64fe319
Daniel Cha <cha.danieldev@gmail.com>
2024-11-21 15:36:27 -0500

    changed browseRouter to hashRouter, may need to research more but it is quickfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 // import { Link } from "react-router-dom";
 import "./App.css";
 import HomeStation from "./pages/homeStation";
