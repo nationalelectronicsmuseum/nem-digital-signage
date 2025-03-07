a481f1021a916c060862ab96d29b4ea3a7135c2d
Daniel Cha <cha.danieldev@gmail.com>
2025-03-07 14:58:54 -0500

    more lang test

--- src/pages/inProgress.jsx
+++ src/pages/inProgress.jsx
@@ -4,21 +4,37 @@ import Menu from "../components/menu/menu.jsx";
 export default function InProgress() {
   const [toggle, setToggle] = useState(false);
 
-  const text = "Hola mundo, me llamo Maria"
+  const textES = "Hola mundo, me llamo Maria"
+  const textFR = "Bonjour le monde, je m'appelle Maria"
+  const textDE = "Hallo Welt, ich bin Maria"
+
 
   const synth = window.speechSynthesis;
-  const utter = new SpeechSynthesisUtterance(text);
+  const utterFrench = new SpeechSynthesisUtterance(textFR);
+  const utterGerman = new SpeechSynthesisUtterance(textDE);
+  const utterSpanish = new SpeechSynthesisUtterance(textES);
 
-  utter.lang = "es-US"
-  utter.pitch = 1.1;
-  utter.rate = 1.2;
-  utter.volume = 1;
+  utterSpanish.lang = "es-US"
+  utterGerman.lang = "de-DE"
+  utterFrench.lang = "fr-FR"
 
-  const speak = () => {
-    synth.speak(utter)
-    console.log("I have been clicked!")
+  const speakFrench = () => {
+    synth.speak(utterFrench)
+    console.log("I have been clicked! Speaking French!")
     console.log("synth", synth)
-    console.log("utterance", utter)
+    console.log("utterance", utterFrench)
+  }
+  const speakGerman = () => {
+    synth.speak(utterGerman)
+    console.log("I have been clicked! Speaking German!")
+    console.log("synth", synth)
+    console.log("utterance", utterGerman)
+  }
+  const speakSpanish = () => {
+    synth.speak(utterSpanish)
+    console.log("I have been clicked! Speaking Spanish!")
+    console.log("synth", synth)
+    console.log("utterance", utterSpanish)
   }
 
   return (
@@ -30,7 +46,9 @@ export default function InProgress() {
                 <h1 className="title">National Electronics Museum</h1>
         </div>
       <div>
-        <button onClick={speak} style={{border: "1px solid black", paddding: "20px", width: "100px", height: "50px"}}>CLick Test</button>
+        <button onClick={speakSpanish} style={{border: "1px solid black", paddding: "20px", width: "100px", height: "50px"}}>CLick Spanish</button>
+        <button onClick={speakGerman} style={{border: "1px solid black", paddding: "20px", width: "100px", height: "50px"}}>CLick German</button>
+        <button onClick={speakFrench} style={{border: "1px solid black", paddding: "20px", width: "100px", height: "50px"}}>CLick French</button>
       </div>
     </div>
   );
