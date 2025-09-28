01eadcf3608ea0be89d35ade6676f6b6704399d4
Docter60 <docter60@hotmail.com>
2025-09-28 18:08:22 -0400

    I24: Add HAM video to display

--- src/pages/hamRadio.jsx
+++ src/pages/hamRadio.jsx
@@ -3,6 +3,8 @@ import Menu from '../components/menu/menu'
 import Accessibility from '../components/accessibility/accessibility'
 import Loop from '../components/loop'
 import AudioSnip from '../components/audioClip/audioSnip'
+import Video from '../components/Video/Video.jsx'
+import hamVideo from "../assets/video/hamRadio.mp4"
 import { getLocalizedArtifact } from "../assets/Localization.js";
 import { HAM_RADIO } from "../assets/ArtifactTags.js";
 
@@ -23,7 +25,7 @@ function HAMRadio() {
   const [five, setFive] = useState("hide")
 
   const artifact = getLocalizedArtifact(HAM_RADIO);
-  
+
   const synth = window.speechSynthesis;
 
   const toggleOne = () => {
@@ -77,108 +79,115 @@ function HAMRadio() {
         </div>
       </div>
 
-      <div className={one}>
-        <div className="hamBox">
-          <div className="infoLeft">
-            <AudioSnip text={artifact.info[0].description}/>
-            <p>{artifact.info[0].description}</p>
-            <div className="audioSnipBox">
-              <AudioSnip text={"Image: A desk with computers, mouse, keyboard, HAM radio, and a map."} />
-              <h5>Image: A desk with computers, mouse, keyboard, HAM radio, and a map.</h5>
+      <div className="hamRadioContainer">
+        <div className="faqGroup">
+          <div className={one}>
+            <div className="hamBox">
+              <div className="infoLeft">
+                <AudioSnip text={artifact.info[0].description} />
+                <p>{artifact.info[0].description}</p>
+                <div className="audioSnipBox">
+                  <AudioSnip text={"Image: A desk with computers, mouse, keyboard, HAM radio, and a map."} />
+                  <h5>Image: A desk with computers, mouse, keyboard, HAM radio, and a map.</h5>
+                </div>
+              </div>
+              <div className="infoRight">
+                <img src={ham1} className="hamImg"></img>
+              </div>
+            </div>
+            <div className="backgroundImage" onClick={toggleOne}></div>
+          </div>
+
+          <div className={two}>
+            <div className="hamBox">
+              <div className="infoLeft">
+                <AudioSnip text={artifact.info[1].description} />
+                <p>{artifact.info[1].description}</p>
+
+                <div className="audioSnipBox">
+                  <AudioSnip text={"Image: A child operating the HAM radio."} />
+                  <h5>Image: A child operating the HAM radio.</h5>
+                </div>
+
+              </div>
+              <div className="infoRight">
+                <img src={ham2} className="hamImg"></img>
+              </div>
+            </div>
+            <div className="backgroundImage" onClick={toggleTwo}></div>
+          </div>
+
+          <div className={three}>
+            <div className="hamBox">
+              <div className="infoLeft">
+                <AudioSnip text={artifact.info[2].description} />
+                <p>{artifact.info[2].description}</p>
+                <div className="audioSnipBox">
+                  <AudioSnip text={"Image: Showing an example of the End Fed."} />
+                  <h5>Image: Showing an example of the End Fed.</h5>
+                </div>
+              </div>
+              <div className="infoRight">
+                <img src={ham3} className="hamImg"></img>
+              </div>
+            </div>
+            <div className="backgroundImage" onClick={toggleThree}></div>
+          </div>
+
+          <div className={four}>
+            <div className="hamBox">
+              <div className="infoLeft">
+                <AudioSnip text={artifact.info[3].description} />
+                <p>{artifact.info[3].description}</p>
+
+                <div className="audioSnipBox">
+                  <AudioSnip text={"Image: ARRL Field Day, an annual emergency preparedness event."} />
+                  <h5>Image: ARRL Field Day, an annual emergency preparedness event.</h5>
+                </div>
+              </div>
+              <div className="infoRight">
+                <img src={ham7} className="hamImg"></img>
+              </div>
+            </div>
+            <div className="backgroundImage" onClick={toggleFour}></div>
+          </div>
+
+          <div className={five}>
+            <div className="backgroundImage" onClick={toggleFive}></div>
+            <div className="hamBox">
+              <div className="infoLeft">
+                <AudioSnip text={artifact.info[4].description} />
+                <p>{artifact.info[4].description}</p>
+
+                <div className="audioSnipBox">
+                  <AudioSnip text={"Scan QR Code to join FB group or explore the website."} />
+                  <h5>Scan QR Code to join FB group or explore the website.</h5>
+                </div>
+              </div>
+              <div className="infoRight">
+                <img src={ham6} className="hamImg"></img>
+              </div>
             </div>
           </div>
-          <div className="infoRight">
-            <img src={ham1} className="hamImg"></img>
-          </div>
-        </div>
-        <div className="backgroundImage" onClick={toggleOne}></div>
-      </div>
 
-      <div className={two}>
-      <div className="hamBox">
-          <div className="infoLeft">
-            <AudioSnip text={artifact.info[1].description}/>
-            <p>{artifact.info[1].description}</p>
-            
-            <div className="audioSnipBox">
-              <AudioSnip text={"Image: A child operating the HAM radio."} />
-              <h5>Image: A child operating the HAM radio.</h5>
-            </div>
-
-          </div>
-          <div className="infoRight">
-          <img src={ham2} className="hamImg"></img>
-          </div>
-        </div>
-        <div className="backgroundImage" onClick={toggleTwo}></div>
-      </div>
-
-      <div className={three}>
-      <div className="hamBox">
-          <div className="infoLeft">
-          <AudioSnip text={artifact.info[2].description}/>
-            <p>{artifact.info[2].description}</p>
-            <div className="audioSnipBox">
-              <AudioSnip text={"Image: Showing an example of the End Fed."} />
-              <h5>Image: Showing an example of the End Fed.</h5>
+          <div className="sectionhead">
+            <div className="sectiontitle">
+              <h1 className="artifactTitle">HAM Radio Club</h1>
             </div>
           </div>
-          <div className="infoRight">
-          <img src={ham3} className="hamImg"></img>
+          <Loop />
+
+          <div className="hamTitles">
+            <button className="hamBtn" onClick={toggleOne}><h2>{artifact.info[0].name}</h2></button>
+            <button className="hamBtn" onClick={toggleTwo}><h2>{artifact.info[1].name}</h2></button>
+            <button className="hamBtn" onClick={toggleThree}><h2 >{artifact.info[2].name}</h2></button>
+            <button className="hamBtn" onClick={toggleFour}><h2 >{artifact.info[3].name}</h2></button>
+            <button className="hamBtn" onClick={toggleFive}><h2 >{artifact.info[4].name}</h2></button>
           </div>
         </div>
-        <div className="backgroundImage" onClick={toggleThree}></div>
-      </div>
-
-      <div className={four}>
-      <div className="hamBox">
-          <div className="infoLeft">
-            <AudioSnip text={artifact.info[3].description}/>
-            <p>{artifact.info[3].description}</p>
-            
-            <div className="audioSnipBox">
-              <AudioSnip text={"Image: ARRL Field Day, an annual emergency preparedness event."} /> 
-              <h5>Image: ARRL Field Day, an annual emergency preparedness event.</h5>
-            </div>
-          </div>
-          <div className="infoRight">
-          <img src={ham7} className="hamImg"></img>
-          </div>
+        <div className="hamVideo">
+          <Video src={hamVideo} width={400} height={711} />
         </div>
-        <div className="backgroundImage" onClick={toggleFour}></div>
-      </div>
-
-      <div className={five}>
-      <div className="backgroundImage" onClick={toggleFive}></div>
-      <div className="hamBox">
-          <div className="infoLeft">
-            <AudioSnip text={artifact.info[4].description}/>
-            <p>{artifact.info[4].description}</p>
-            
-            <div className="audioSnipBox">
-            <AudioSnip text={"Scan QR Code to join FB group or explore the website."} /> 
-              <h5>Scan QR Code to join FB group or explore the website.</h5>
-            </div>
-          </div>
-          <div className="infoRight">
-          <img src={ham6} className="hamImg"></img>
-          </div>
-        </div>
-      </div>
-
-      <div className="sectionhead">
-        <div className="sectiontitle">
-          <h1 className="artifactTitle">HAM Radio Club</h1>
-        </div>
-      </div>
-      <Loop />
-
-      <div className="hamTitles">
-        <button className="hamBtn" onClick={toggleOne}><h2>{artifact.info[0].name}</h2></button>
-        <button className="hamBtn" onClick={toggleTwo}><h2>{artifact.info[1].name}</h2></button>
-        <button className="hamBtn" onClick={toggleThree}><h2 >{artifact.info[2].name}</h2></button>
-        <button className="hamBtn" onClick={toggleFour}><h2 >{artifact.info[3].name}</h2></button>
-        <button className="hamBtn" onClick={toggleFive}><h2 >{artifact.info[4].name}</h2></button>
       </div>
     </div>
   )
