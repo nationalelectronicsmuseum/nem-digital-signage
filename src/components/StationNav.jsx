b2c7c1f651c491e3ad324411ebccd9b2790cfbb5
Docter60 <docter60@hotmail.com>
2025-12-24 23:27:49 -0500

    Fix navigation menu to grab localisation section titles

--- src/components/StationNav.jsx
+++ src/components/StationNav.jsx
@@ -1,21 +1,28 @@
 import { Link } from "react-router-dom";
+import { loadContent } from "../utils/loadContent";
+import { useSettings } from "../context/SettingsContext.jsx";
+import { resolvePath } from "../utils/resolvePath.js";
 import "../styles/StationNav.css";
 
 export default function StationNav({ station }) {
+  const { settings } = useSettings();
+  const languageCode = settings.language.languageCode;
+  const content = loadContent(languageCode);
+
   return (
     <nav>
-      <Link to={"/" + station.id}>
-        Home
+      <Link to={"/" + station.id}>Home</Link>
+      {station.sections.map((sec) => {
+        let title = resolvePath(content, "common.sectionTitle." + sec.id);
+        return (
+          <Link key={sec.id} to={"/" + station.id + "/" + sec.id}>
+            {title}
+          </Link>
+        );
+      })}
+      <Link to={"/"}>
+        <div className="transparent">Index</div>
       </Link>
-      {station.sections.map((sec) => (
-        <Link
-          key={sec.id}
-          to={"/" + station.id + "/" + sec.id}
-        >
-          {sec.title}
-        </Link>
-      ))}
-      <Link to={"/"}><div className="transparent">Index</div></Link>
     </nav>
   );
 }
