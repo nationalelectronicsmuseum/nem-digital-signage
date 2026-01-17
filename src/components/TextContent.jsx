bbd9dcafa275a1f32264e0b368765d02f65f8270
Brian Boellner <docter60@hotmail.com>
2026-01-17 16:22:44 -0500

    Add vocabulary feature to text content

--- src/components/TextContent.jsx
+++ src/components/TextContent.jsx
@@ -1,9 +1,17 @@
 import { useSettings } from "../context/SettingsContext.jsx";
+import { useState } from "react";
 import SpeechPlaybackControls from "./SpeechPlaybackControls.jsx";
+import VocabularyOverlay from "./VocabularyOverlay.jsx";
+import { loadContent } from "../utils/loadContent";
+import { resolvePath } from "../utils/resolvePath.js";
 import "../styles/TextContent.css";
 
 export default function TextContent({ componentObject }) {
   const { settings } = useSettings();
+  const languageCode = settings.language.languageCode;
+  const content = loadContent(languageCode);
+  let vocabulary = resolvePath(content, "vocabulary");
+  const [activeWord, setActiveWord] = useState(null);
   const aStyle = {
     fontSize: settings.fontSize.point,
     fontCode: settings.font.fontCode,
@@ -19,6 +27,8 @@ export default function TextContent({ componentObject }) {
   const text = componentObject.text;
   const speechText = label != null ? label + " " + text : text;
 
+  const tokens = text.split(/\b/);
+
   return (
     <div className="text-content">
       <span style={labelStyle} className="text-content-label">
@@ -26,7 +36,30 @@ export default function TextContent({ componentObject }) {
         <SpeechPlaybackControls text={speechText} />
       </span>
       <p style={aStyle} className="text-content-text">
-        {text}
+        {tokens.map((token, i) => {
+        const key = token.toLowerCase();
+
+        if (vocabulary[key]) {
+          return (
+            <span
+              key={i}
+              className="vocabulary-word"
+              onClick={() => setActiveWord(vocabulary[key])}
+            >
+              {token}
+            </span>
+          );
+        }
+        return token;
+      })}
+
+      {activeWord && (
+        <VocabularyOverlay
+          term={activeWord.term}
+          definition={activeWord.definition}
+          onClose={() => setActiveWord(null)}
+        />
+      )}
       </p>
     </div>
   );
